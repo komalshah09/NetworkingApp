1d7c4f94f4319fd82114078b5ca81f4be956f44a
rajwindersidhu92 <rajwin.kaur5@gmail.com>
2022-07-05 13:25:11 -0400

    ng_build_done_again

--- API/wwwroot/assets/scripts.js
+++ API/wwwroot/assets/scripts.js
@@ -29,31 +29,4 @@ window.addEventListener('DOMContentLoaded', event => {
     // Shrink the navbar when page is scrolled
     document.addEventListener('scroll', navbarShrink);
 
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            offset: 74,
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                navbarToggler.click();
-            }
-        });
-    });
-
-    // Activate SimpleLightbox plugin for portfolio items
-    new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
-    });
-
 });
---
+++ API/wwwroot/scripts.708b8348e0c14b3ee442.js
@@ -0,0 +1,6 @@
+/*!
+* Start Bootstrap - Creative v7.0.6 (https://startbootstrap.com/theme/creative)
+* Copyright 2013-2022 Start Bootstrap
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-creative/blob/master/LICENSE)
+*/
+window.addEventListener("DOMContentLoaded",n=>{var e=function(){const n=document.body.querySelector("#mainNav");n&&(0===window.scrollY?n.classList.remove("navbar-shrink"):n.classList.add("navbar-shrink"))};e(),document.addEventListener("scroll",e)});
